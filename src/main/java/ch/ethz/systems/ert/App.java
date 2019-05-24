9cdae92c4e7ebf284733be1dabaae17838318b09
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2019-05-24 10:19:50 +0200

    initial commit. Empty mvn project.

---
+++ src/main/java/ch/ethz/systems/ert/App.java
@@ -0,0 +1,13 @@
+package ch.ethz.systems.ert;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
